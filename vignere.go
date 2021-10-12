8523e11f0d388f3826dad2d5e7a2ef5e7c4e29a0
Yvonne Kim <me@yvonne.kim>
2021-10-12 16:55:42 -0500

    Add detection and frequency detection

--- vignere.go
+++ vignere.go
@@ -11,6 +11,36 @@ import (
 
 var alphabet string
 
+// http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+var etaoinsrhdlucmfywgpbvkxqjz = map[rune]int{
+	'e': 1202,
+	't': 910,
+	'a': 812,
+	'o': 768,
+	'i': 731,
+	'n': 695,
+	's': 628,
+	'r': 602,
+	'h': 592,
+	'd': 432,
+	'l': 398,
+	'u': 288,
+	'c': 271,
+	'm': 261,
+	'f': 230,
+	'y': 211,
+	'w': 209,
+	'g': 203,
+	'p': 182,
+	'b': 149,
+	'v': 111,
+	'k': 69,
+	'x': 17,
+	'q': 11,
+	'j': 10,
+	'z': 7,
+}
+
 func help() {
 	fmt.Println(`
 		Examples:
@@ -52,7 +82,24 @@ func getConfig() Config {
 	return config
 }
 
-func get_frequencies(ciphertext string, key_len int) map[rune]int {
+func detect_offset(frequencies map[rune]int) int {
+	var most_frequent_letter rune
+	var highest_count int
+	for letter, count := range frequencies {
+		if count > highest_count {
+			most_frequent_letter = letter
+			highest_count = count
+		}
+	}
+
+	index_difference := int(most_frequent_letter) - int('e')
+	if index_difference < 0 {
+		index_difference = int('e') - int(most_frequent_letter)
+	}
+	return index_difference
+}
+
+func count_frequencies(ciphertext string, key_len int) map[rune]int {
 	result := make(map[rune]int)
 	for i := 0; i < len(ciphertext); i += key_len {
 		result[rune(ciphertext[i])] += 1
@@ -62,15 +109,17 @@ func get_frequencies(ciphertext string, key_len int) map[rune]int {
 
 func solve(ciphertext string) string {
 	for key_len := 1; key_len < 10; key_len++ {
-		freqs := get_frequencies(ciphertext, key_len)
-		fmt.Println(freqs)
+		freqs := count_frequencies(ciphertext, key_len)
+		offset := detect_offset(freqs)
+		offset_letter := string(int('a') + offset)
 	}
+
 	return ""
 }
 
 func sanitize(text string) string {
 	result := ""
-	for _, letter := range text {
+	for _, letter := range strings.ToLower(text) {
 		if letter < 97 || letter > 122 {
 			continue
 		}
@@ -82,7 +131,6 @@ func sanitize(text string) string {
 
 func encode(plaintext string, keyword string) string {
 	result := ""
-	plaintext = strings.ToLower(plaintext)
 	plaintext = sanitize(plaintext)
 	for i, letter := range plaintext {
 		letter_index := strings.Index(alphabet, string(letter))
